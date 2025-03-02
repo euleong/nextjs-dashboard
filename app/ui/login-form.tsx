7842d759fc45d9896cf215408d87dc886e69d49b
euleong <eugenia.leong@udemy.com>
2025-03-02 15:37:06 -0800

    Fix lint

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -9,10 +9,10 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
-import { useSearchParams } from 'next/navigation';
+// import { useSearchParams } from 'next/navigation';
 
 export default function LoginForm() {
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
   // const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
   const [errorMessage, formAction] = useActionState(authenticate, undefined);
 
